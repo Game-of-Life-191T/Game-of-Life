bcda1646a9ae626c417d2c567e896b2a40f91230
Michael Spohn <michaelspohn97@outlook.com>
2021-04-01 16:04:15 -0700

    Input for custom rules
    Simple while loop for setting the rule number. Need to convert to binary list.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Test")
+# ------------------------- #
+# Game of Life 191T Project #
+# ------------------------- #
+#     Mitchell Maltezo      #
+#      Michael Spohn        #
+#     Rosellyn Vicente      #
+#       Pranav Arora        #
+# ------------------------- #
+
+# Custom rule set
+rule = -1
+while rule < 1 or rule > 256:
+    print("Enter rule number (1-256):")
+    rule = input()
+    rule = int(rule)
+    if rule > 0 and rule < 257:
+        rule = rule - 1
+        break
+    print("Error, please retype your input")
+
+setrule = []
